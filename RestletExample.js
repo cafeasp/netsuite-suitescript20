528b9490643e9c8dac8b9388e9a098d311673b43
cafesito <victor@cafecustomink.com>
2019-05-25 12:55:29 -0400

    Create Sales Order Video
    Post a order in json format and create a sales order.

--- RestletExample.js
+++ RestletExample.js
@@ -29,7 +29,7 @@ define(['N/search', 'N/record'],
          * @since 2015.2
          */
         function doPut(requestBody) {
-            
+
         }
 
         /**
@@ -41,7 +41,12 @@ define(['N/search', 'N/record'],
          * @since 2015.2
          */
         function doPost(requestBody) {
-           
+            var salesOrderId = CreateNetSuiteOrder(requestBody);
+            if (salesOrderId > 0) {
+                log.debug('SO', salesOrderId);
+            } else {
+                log.debug('SO', 'Fail');
+            }
         }
 
         /**
@@ -52,6 +57,85 @@ define(['N/search', 'N/record'],
          * @since 2015.2
          */
         function doDelete(requestParams) {
+
+        }
+
+
+        function CreateNetSuiteOrder(requestBody) {
+
+            log.debug('Post body', requestBody);
+
+            var salesOrder = record.create({
+                type: record.Type.SALES_ORDER,
+                isDynamic: true,
+                defaultValues: {
+                    entity: 123242440099
+                }
+            });
+
+
+            salesOrder.setValue({ fieldId: 'trandate', value: new Date('5/25/2019') });
+
+            var subrec = salesOrder.getSubrecord({
+                fieldId: 'shippingaddress'
+            });
+
+            subrec.setValue({ fieldId: 'addr1', value: '123 street' });
+            subrec.setValue({ fieldId: 'city', value: 'city' });
+            subrec.setValue({ fieldId: 'state', value: 'state' });
+            subrec.setValue({ fieldId: 'zip', value: 'CA' });
+            subrec.setValue({ fieldId: 'addressee', value: 'John' });
+
+            subrec.setValue({ fieldId: 'attention', value: 'John' });
+
+
+            salesOrder.selectNewLine({
+                sublistId: 'item'
+            });
+
+
+            var items = requestBody.items;
+            items.forEach(function (item) {
+
+                salesOrder.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'item',
+                    value: '4353535334535'//internal id 
+                });
+
+                salesOrder.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'quantity',
+                    value: item.quantity_order
+                });
+                salesOrder.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'price',
+                    value: -1 //custom in netsuite
+                });
+
+                salesOrder.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'rate',
+                    value: item.price_value
+                });
+
+                salesOrder.commitLine({
+                    sublistId: 'item'
+                });
+            });
+
+            try {
+                var id = salesOrder.save({
+                    ignoreMandatoryFields: false
+                });
+                log.debug('record save with id', id);//sales order internal id
+
+                return id;
+            } catch (e) {
+                return 0;
+            }
+
 
         }
 
